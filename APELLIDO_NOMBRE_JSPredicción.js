6e1a815bd52645ab4241f5617b219766696bf6ba
maximilianoxdd <programacionclase12@gmail.com>
2022-08-12 12:38:47 -0400

    js

---
+++ APELLIDO_NOMBRE_JSPredicción.js
@@ -0,0 +1,22 @@
+//Ejercicio 1
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980); // naci en 1980
+}copy 
+
+//ejercicio 2
+var EntradaAñoNacimiento = 1980;
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento); // naci en 1980
+}copy
+
+//ejercicio 3
+function add(num1, num2){
+        console.log("¡Sumando números!"); //¡Sumando números!
+        console.log("num1 is: " + num1); // num1 is: 10 
+        console.log("num2 is: " + num2); // num2 is: 20
+        var sum = num1 + num2;
+        console.log(sum); //30
+    }
+    
+    
