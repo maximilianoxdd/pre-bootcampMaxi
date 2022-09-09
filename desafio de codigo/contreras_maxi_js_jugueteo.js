6e0c1d3d5ad9c8d7a3bb64c621caf04625c533c2
maximilianoxdd <programacionclase12@gmail.com>
2022-09-09 12:04:36 -0300

    opcional js

---
+++ desafio de codigo/contreras_maxi_js_jugueteo.js
@@ -0,0 +1,15 @@
+var alturaniño = 56;
+
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaniño){
+    
+
+
+   if(alturaniño > 56){
+    console.log("¡subete, chico!")
+    }else{
+        console.log("lo siento, chico. Tal vez el proximo año")
+       }
+
+}
+
+
